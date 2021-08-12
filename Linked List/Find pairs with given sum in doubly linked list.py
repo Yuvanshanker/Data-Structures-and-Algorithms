5ec1756cbd580a741d91cbdd6937a9d3516e4fba
Yuvanshanker <65071471+Yuvanshanker@users.noreply.github.com>
2021-08-12 23:10:39 +0530

    Create Find pairs with given sum in doubly linked list.py

---
+++ Linked List/Find pairs with given sum in doubly linked list.py
@@ -0,0 +1,80 @@
+class Node:
+     
+    def __init__(self, x):
+         
+        self.data = x
+        self.next = None
+        self.prev = None
+ 
+# Function to find pair whose sum
+# equal to given value x.
+def pairSum(head, x):
+     
+    # Set two pointers, first to the
+    # beginning of DLL and second to
+    # the end of DLL.
+    first = head
+    second = head
+     
+    while (second.next != None):
+        second = second.next
+ 
+    # To track if we find a pair or not
+    found = False
+ 
+    # The loop terminates when they
+    # cross each other (second.next ==
+    # first), or they become same
+    # (first == second)
+    while (first != second and second.next != first):
+             
+        # Pair found
+        if ((first.data + second.data) == x):
+            found = True
+            print("(", first.data, ",",
+                       second.data, ")")
+             
+            # Move first in forward direction
+            first = first.next
+             
+            # Move second in backward direction
+            second = second.prev
+        else:
+            if ((first.data + second.data) < x):
+                first = first.next
+            else:
+                second = second.prev
+ 
+    # If pair is not present
+    if (found == False):
+        print("No pair found")
+ 
+# A utility function to insert a new node
+# at the beginning of doubly linked list
+def insert(head, data):
+     
+    temp = Node(data)
+     
+    if not head:
+        head = temp
+    else:
+        temp.next = head
+        head.prev = temp
+        head = temp
+         
+    return head
+ 
+# Driver code
+if __name__ == '__main__':
+     
+    head = None
+    head = insert(head, 9)
+    head = insert(head, 8)
+    head = insert(head, 6)
+    head = insert(head, 5)
+    head = insert(head, 4)
+    head = insert(head, 2)
+    head = insert(head, 1)
+    x = 7
+ 
+    pairSum(head, x)
